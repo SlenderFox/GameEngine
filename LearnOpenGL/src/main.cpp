f6ac068877b619d1586e1f116117f034bcc60fbe
SlenderFox <SlenderFox@outlook.com>
2021-06-30 17:38:49 +1000

    Basic window rendering

---
+++ LearnOpenGL/src/main.cpp
@@ -0,0 +1,88 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#define WAITFORINPUT true;
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    // glfw: initialise and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Already set by default
+    //glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    //glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+    //glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+    //glfwWindowHint(GLFW_REFRESH_RATE, GLFW_DONT_CARE);
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Input
+        processInput(window);
+
+        // Rendering
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    // Once the window has closed keep the console open
+#ifdef WAITFORINPUT
+    // Waits for any input
+    std::cout << "Press enter to close...";
+    std::cin.clear();
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
+#else
+    // Waits 5 seconds
+    //int closeTime = glfwGetTime() + 5;
+    //while (glfwGetTime() < closeTime) { }
+#endif
+
+	return 0;
+}
+
+// Called when the user resizes the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_END) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
