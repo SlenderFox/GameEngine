141823fff831a2a556f0a41a52f49848e2e297b5
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2022-03-31 21:41:24 +1100

    Starting on entities
    Vscode is being very odd and intellisense breaks when the file is named "Entity"

--- LearnOpenGL/src/Entity.cpp
+++ LearnOpenGL/src/Entity.cpp
@@ -2,5 +2,24 @@
 
 namespace Engine
 {
-    
+    Entity::Entity()
+    {
+        m_children = new vector<Entity*>();
+    }
+
+    Entity::Entity(Entity* pParent)
+    {
+        m_parent = pParent;
+        m_children = new vector<Entity*>();
+    }
+
+    Entity* Entity::GetParent() const
+    {
+        return m_parent;
+    }
+
+    vector<Entity*>* Entity::GetChildren() const
+    {
+        return m_children;
+    }
 }
--- LearnOpenGL/src/Entity.hpp
+++ LearnOpenGL/src/Entity.hpp
@@ -1,10 +1,22 @@
 #pragma once
 #include "Transform.hpp"
+#include <vector>
+
+using std::vector;
 
 namespace Engine
 {
     class Entity : public Transform
     {
+    public:
+        Entity();
+        Entity(Entity* pParent);
 
+        Entity* GetParent() const;
+        vector<Entity*>* GetChildren() const;
+
+    private:
+        Entity* m_parent = nullptr;
+        vector<Entity*>* m_children = nullptr;
     };
 }
