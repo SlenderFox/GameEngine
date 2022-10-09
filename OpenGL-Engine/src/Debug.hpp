3ff30d0526644861e5b669327548d23e50088135
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2022-10-09 23:05:25 +1100

    Added comment for permutations of chars
    Why does it not work for ascii above 197. I am fucking losing it I can't even move to utf since it's just even more broken and I can't print

--- OpenGL-Engine/src/Debug.hpp
+++ OpenGL-Engine/src/Debug.hpp
@@ -11,69 +11,81 @@ namespace Engine
 		static void PrintMsg(char pChar, std::string& pMsg, bool pNewline, bool pEndline) noexcept
 		{
 			if (pNewline) std::cout << '\n';
-			std::cout << ' ' << pChar << pMsg;
+			printf(" %c%s", pChar, pMsg.c_str());
 			if (pEndline) std::cout << std::endl;
 		}
 
 	public:
 		static void InitSmallNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ┌ \xDA 218
 			PrintMsg('\xDA', pMsg, pNewline, pEndline);
 		}
 
 		static void InitBigNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ╒ \xD5 213
 			PrintMsg('\xD5', pMsg, pNewline, pEndline);
 		}
 
 		static void InitSmallProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ┬ \xC2 194
 			PrintMsg('\xC2', pMsg, pNewline, pEndline);
 		}
 
 		static void InitBigProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ╤ \xD1 209
 			PrintMsg('\xD1', pMsg, pNewline, pEndline);
 		}
 
 		static void SmallNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ├ \xC3 195
 			PrintMsg('\xC3', pMsg, pNewline, pEndline);
 		}
 
 		static void BigNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ╞ \xC6 198
 			PrintMsg('\xC6', pMsg, pNewline, pEndline);
 		}
 
 		static void SmallProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ┼ \xC5 197
 			PrintMsg('\xC5', pMsg, pNewline, pEndline);
 		}
 
 		static void BigProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ╪ \xD8 216
 			PrintMsg('\xD8', pMsg, pNewline, pEndline);
 		}
 
 		static void EndSmallNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// └ \xC0 192
 			PrintMsg('\xC0', pMsg, pNewline, pEndline);
 		}
 
 		static void EndBigNote(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ╘ \xD4 212
 			PrintMsg('\xD4', pMsg, pNewline, pEndline);
 		}
 
 		static void EndSmallProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
+			// ┴ \xC1 193
 			PrintMsg('\xC1', pMsg, pNewline, pEndline);
 		}
 
 		static void EndBigProcess(std::string pMsg, bool pNewline = true, bool pEndline = true) noexcept
 		{
-			PrintMsg('\xD0', pMsg, pNewline, pEndline);
+			// ╧ \xCF 207
+			PrintMsg('\xCF', pMsg, pNewline, pEndline);
 		}
 	};
 }
