bf2965223a98a467d63e47a23c87233d0cca2b54
SlenderFox <SlenderFox@outlook.com>
2022-11-22 23:32:50 +1100

    Entry point moved into Project.cpp
    main.cpp is no more

--- Example/src/Project.cpp
+++ Example/src/Project.cpp
@@ -17,6 +17,33 @@ using Engine::Renderer;
 using Engine::Input;
 #pragma endregion
 
+/** Entry point of the project
+ * @note Following: https://learnopengl.com/
+ * @note Disable console through:
+ * [Properties>Linker>System>Subsystem & Properties>Linker>Advanced>Entry Point (mainCRTStartup)]
+ * @note Resolutions:
+ * 800, 600
+ * 1280, 720
+ * 1600, 900
+ * 1920, 1080
+ * @note Refer to Notes.md to see TODO and notes
+ */
+int main()
+{
+	Project* app = new Project();
+	#ifdef _DEBUG
+		app->SetDimensions(1030, 650);
+		app->SetTitle("Application (DEBUG)");
+	#else
+		app->SetDimensions(1600, 900);
+		app->SetTitle("Application (RELEASE)");
+	#endif
+	app->SetFullscreen(false);
+	int code = (int)app->Run();
+	delete app;
+	return code;
+}
+
 // Static
 
 double Project::s_camYaw = 90.0;
