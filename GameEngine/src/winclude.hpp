d084931f538916dfd7f3e7a2668b517b70b302f2
SlenderFox <SlenderFox@outlook.com>
2023-01-18 16:47:22 +1100

    debug now only moves the console on windows platforms

--- GameEngine/src/winclude.hpp
+++ GameEngine/src/winclude.hpp
@@ -1,19 +1,30 @@
 #pragma once
 
-#if defined(WIN32) || defined(_WIN32)
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__)
 
 	#define WIN32_LEAN_AND_MEAN
+
+	#ifndef WIN32
+		#define WIN32
+	#endif
+	#ifndef _WIN32
+		#define _WIN32
+	#endif
+	#ifndef __WIN32__
+		#define __WIN32__
+	#endif
+	#ifndef __WINDOWS__
+		#define __WINDOWS__
+	#endif
 	#ifndef UNICODE
 		#define UNICODE
 	#endif
 	#ifndef STRICT
 		#define STRICT
 	#endif
-
-	#ifdef NOMINMAX
-		#include <Windows.h>
-	#else
+	#ifndef NOMINMAX
 		#define NOMINMAX
-		#include <Windows.h>
 	#endif
+
+	#include <Windows.h>
 #endif
