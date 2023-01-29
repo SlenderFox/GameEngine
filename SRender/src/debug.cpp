77078f4dc0c96940a3bc68139167ac390fc8fdc6
SlenderFox <SlenderFox@outlook.com>
2023-01-29 15:02:57 +1100

    Fixed imgui includes

--- SRender/src/debug.cpp
+++ SRender/src/debug.cpp
@@ -1,7 +1,7 @@
 #include "debug.hpp"
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_glfw.h"
-#include "../imgui/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #ifdef _DEBUG
 	#include "winclude.hpp"
