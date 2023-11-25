2097339ee8c73bf6898f6e7a73484e20d3bd8c0e
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2023-11-26 01:32:17 +1100

    Added colour constexpr to colour struct
    I accidentlly had them redefined in the srender namespace

--- SRender/colour.cpp
+++ SRender/colour.cpp
@@ -159,23 +159,23 @@ constexpr colour::colour(colour&& _other) noexcept
 constexpr colour &colour::operator=(colour&& _other) noexcept
 {	this->m_RGB = std::move(_other.m_RGB); return *this; }
 
-colour black()    noexcept { return colour(0.0f); }
-colour white()    noexcept { return colour(1.0f); }
-colour silver()   noexcept { return colour(0.75f, 0.75f, 0.75f ); }
-colour grey()     noexcept { return colour(0.5f,  0.5f,  0.5f  ); }
-colour darkGrey() noexcept { return colour(0.25f, 0.25f, 0.25f ); }
-colour red()      noexcept { return colour(1.0f,  0.0f,  0.0f  ); }
-colour lime()     noexcept { return colour(0.0f,  1.0f,  0.0f  ); }
-colour blue()     noexcept { return colour(0.0f,  0.0f,  1.0f  ); }
-colour yellow()   noexcept { return colour(1.0f,  1.0f,  0.0f  ); }
-colour cyan()     noexcept { return colour(0.0f,  1.0f,  1.0f  ); }
-colour magenta()  noexcept { return colour(1.0f,  0.0f,  1.0f  ); }
-colour maroon()   noexcept { return colour(0.5f,  0.0f,  0.0f  ); }
-colour green()    noexcept { return colour(0.0f,  0.5f,  0.0f  ); }
-colour navy()     noexcept { return colour(0.0f,  0.0f,  0.5f  ); }
-colour olive()    noexcept { return colour(0.5f,  0.5f,  0.0f  ); }
-colour teal()     noexcept { return colour(0.0f,  0.5f,  0.5f  ); }
-colour purple()   noexcept { return colour(0.5f,  0.0f,  0.5f  ); }
+colour colour::black()    noexcept { return colour(0.0f); }
+colour colour::white()    noexcept { return colour(1.0f); }
+colour colour::silver()   noexcept { return colour(0.75f, 0.75f, 0.75f ); }
+colour colour::grey()     noexcept { return colour(0.5f,  0.5f,  0.5f  ); }
+colour colour::darkGrey() noexcept { return colour(0.25f, 0.25f, 0.25f ); }
+colour colour::red()      noexcept { return colour(1.0f,  0.0f,  0.0f  ); }
+colour colour::lime()     noexcept { return colour(0.0f,  1.0f,  0.0f  ); }
+colour colour::blue()     noexcept { return colour(0.0f,  0.0f,  1.0f  ); }
+colour colour::yellow()   noexcept { return colour(1.0f,  1.0f,  0.0f  ); }
+colour colour::cyan()     noexcept { return colour(0.0f,  1.0f,  1.0f  ); }
+colour colour::magenta()  noexcept { return colour(1.0f,  0.0f,  1.0f  ); }
+colour colour::maroon()   noexcept { return colour(0.5f,  0.0f,  0.0f  ); }
+colour colour::green()    noexcept { return colour(0.0f,  0.5f,  0.0f  ); }
+colour colour::navy()     noexcept { return colour(0.0f,  0.0f,  0.5f  ); }
+colour colour::olive()    noexcept { return colour(0.5f,  0.5f,  0.0f  ); }
+colour colour::teal()     noexcept { return colour(0.0f,  0.5f,  0.5f  ); }
+colour colour::purple()   noexcept { return colour(0.5f,  0.0f,  0.5f  ); }
 
 colour::operator glm::vec3() const
 {	return m_RGB; }
