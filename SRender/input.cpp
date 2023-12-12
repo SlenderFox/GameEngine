7d9f3388a8f67b2e5e529ee69644409505dbf72e
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2023-12-12 18:27:43 +1100

    Fixing the first frame jank

--- SRender/input.cpp
+++ SRender/input.cpp
@@ -35,15 +35,28 @@ namespace input
 		double _posY
 	) noexcept
 	{
-		l_mouseLastX = l_mouseX;
-		l_mouseLastY = l_mouseY;
-		l_mouseX = _posX;
-		l_mouseY = _posY;
-		double deltaX = l_mouseX - l_mouseLastX;
-		double deltaY = l_mouseY - l_mouseLastY;
-
-		if (l_mouseCallbackFun)
-		{	l_mouseCallbackFun(deltaX, deltaY); }
+		//debug::send(std::to_string(_posX) + "," + std::to_string(_posY));
+		// First frame
+		if (l_mouseLastX == 0.0 && l_mouseLastY == 0.0)
+		{
+			l_mouseX = _posX;
+			l_mouseLastX = _posX;
+			l_mouseY = _posY;
+			l_mouseLastY = _posY;
+			if (l_mouseCallbackFun)
+			{	l_mouseCallbackFun(0.0, 0.0); }
+		}
+		else
+		{
+			l_mouseLastX = l_mouseX;
+			l_mouseLastY = l_mouseY;
+			l_mouseX = _posX;
+			l_mouseY = _posY;
+			double deltaX = l_mouseX - l_mouseLastX;
+			double deltaY = l_mouseY - l_mouseLastY;
+			if (l_mouseCallbackFun)
+			{	l_mouseCallbackFun(deltaX, deltaY); }
+		}
 	}
 
 	void scroll_callback(
@@ -60,13 +73,13 @@ namespace input
 	{
 		l_windowRef = _windowRef;
 
-		glfwGetCursorPos(l_windowRef, &l_mouseX, &l_mouseY);
-		l_mouseLastX = l_mouseX;
-		l_mouseLastY = l_mouseY;
+		// glfwGetCursorPos(l_windowRef, &l_mouseX, &l_mouseY);
+		// l_mouseLastX = l_mouseX;
+		// l_mouseLastY = l_mouseY;
 
 		glfwSetInputMode(l_windowRef, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 		if (glfwRawMouseMotionSupported())
-			glfwSetInputMode(l_windowRef, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+		{	glfwSetInputMode(l_windowRef, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE); }
 
 		glfwSetKeyCallback(l_windowRef, key_callback);
 		glfwSetCursorPosCallback(l_windowRef, mouse_callback);
